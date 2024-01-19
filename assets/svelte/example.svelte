a935076f7e86a3bf7de8d6fdf9a8f9723d9e0352
Dino Danic <dino.danic@gmail.com>
2024-01-19 22:12:18 +0100

    add shadcn

--- assets/svelte/example.svelte
+++ assets/svelte/example.svelte
@@ -1,2 +1,6 @@
-<div class="bg-red-500 text-sky-600"> asdf ja sam svelte</div>
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button";
+</script>
 
+<div class="text-sky-600 bg-primary">asdf ja sam svelte</div>
+<Button>bok</Button>
--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,6 +1,11 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 
+import { fontFamily } from "tailwindcss/defaultTheme";
+// /** @type {import('tailwindcss').Config} */
+// const config = {};
+// export default config;
+
 const plugin = require("tailwindcss/plugin");
 const fs = require("fs");
 const path = require("path");
@@ -12,11 +17,61 @@ module.exports = {
     "../lib/snap_web/**/*.*ex",
     "./svelte/**/*.svelte",
     "./src/**/*.{html,js,svelte,ts}",
+    "./src/**/*.{html,js,svelte,ts}",
   ],
+  darkMode: ["class"],
+  safelist: ["dark"],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
       colors: {
-        brand: "#FD4F00",
+        border: "hsl(var(--border) / <alpha-value>)",
+        input: "hsl(var(--input) / <alpha-value>)",
+        ring: "hsl(var(--ring) / <alpha-value>)",
+        background: "hsl(var(--background) / <alpha-value>)",
+        foreground: "hsl(var(--foreground) / <alpha-value>)",
+        primary: {
+          DEFAULT: "hsl(var(--primary) / <alpha-value>)",
+          foreground: "hsl(var(--primary-foreground) / <alpha-value>)",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary) / <alpha-value>)",
+          foreground: "hsl(var(--secondary-foreground) / <alpha-value>)",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive) / <alpha-value>)",
+          foreground: "hsl(var(--destructive-foreground) / <alpha-value>)",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted) / <alpha-value>)",
+          foreground: "hsl(var(--muted-foreground) / <alpha-value>)",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent) / <alpha-value>)",
+          foreground: "hsl(var(--accent-foreground) / <alpha-value>)",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover) / <alpha-value>)",
+          foreground: "hsl(var(--popover-foreground) / <alpha-value>)",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card) / <alpha-value>)",
+          foreground: "hsl(var(--card-foreground) / <alpha-value>)",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      fontFamily: {
+        sans: ["Inter", ...fontFamily.sans],
       },
     },
   },
@@ -55,7 +110,7 @@ module.exports = {
     // Embeds Heroicons (https://heroicons.com) into your app.css bundle
     // See your `CoreComponents.icon/1` for more information.
     //
-    plugin(function ({ matchComponents, theme }) {
+    plugin(function({ matchComponents, theme }) {
       let iconsDir = path.join(__dirname, "./vendor/heroicons/optimized");
       let values = {};
       let icons = [

8d6d20c9de3b0cce9d85c67d48bd21479c9095e9
Dino Danic <dino.danic@gmail.com>
2024-01-28 16:24:04 +0100

    docker;

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
-/app/snap/bin/snap eval "ReleaseTasks.migrate"
+#
+/app/snap/bin/snap eval "RelaseTasks.migrate"
 /app/snap/bin/snap start
 
+# /app/holiday_shops/bin/holiday_shops eval "HolidayShops.ReleaseTasks.migrate"
+# /app/holiday_shops/bin/holiday_shops start
+#
+# mix ecto.migrate && mix phx.server
